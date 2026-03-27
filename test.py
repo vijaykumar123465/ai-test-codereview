d33972fd71eb768aaf315ea926d25035a7217dc4
vijaykumar123465 <vijayvijayv885@gmail.com>
2026-03-27 08:06:54 +0530

    comited

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-password =123456789899
-name="vijaykumar"
+import os
+
+# Bad: hardcoded password
+DATABASE_PASSWORD = "super_secret_123"
+
+# Bad: hardcoded discount
+DISCOUNT_RATE = 0.20
+
+# Bad: potential division by zero
+def calculate_average(total, count):
+    return total / count
+
+# Good: using environment variable
+API_KEY = os.getenv("API_KEY")
