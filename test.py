8cfe95df7d426a905bbe43eeed71358b4b801477
vijaykumar123465 <vijayvijayv885@gmail.com>
2026-04-02 09:16:23 +0530

    my commit

--- test.py
+++ test.py
@@ -11,7 +11,5 @@ def calculate_average(total, count):
     return total / count ;
 
 # Good: using environment variable
-API_KEY = os.getenv("API_KEY"):::::::
-dasdfg=grgrg
-
-
+API_KEY = os.getenv("API_KEY")
+name==1234;
