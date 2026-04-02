0d6b00b61501f2a7b0400e661febaa1a8bfdadfa
vijaykumar123465 <vijayvijayv885@gmail.com>
2026-04-02 09:12:22 +0530

    my commit

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ import os
 DATABASE_PASSWORD = "super_secret_123"
  
 # Bad: hardcoded discount
-DISCOUNT_RATE = 0.20%
+DISCOUNT_RATE = 0.20
 
 # Bad: potential division by zero
 def calculate_average(total, count):
     return total / count ;
 
 # Good: using environment variable
-API_KEY = os.getenv("API_KEY"):::
+API_KEY = os.getenv("API_KEY");
 
 
