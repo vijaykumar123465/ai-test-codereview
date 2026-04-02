0c96757657500c49ba59a3b16a9fc0d6df0d8658
vijaykumar123465 <vijayvijayv885@gmail.com>
2026-04-02 12:30:28 +0530

    my commit

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ DISCOUNT_RATE = 10%:
 
 # Bad: potential division by zero
 def calculate_average(total, count):
-    return total / count ;
+    return total / count :
 
 # Good: using environment variable
 API_KEY = os.getenv("API_KEY")
