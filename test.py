306a05822c3a21895e33f54b0d24ca35a7b9ba33
vijaykumar123465 <vijayvijayv885@gmail.com>
2026-05-07 15:13:31 +0530

    hello

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
 import os
-
-# Bad: hardcoded password
 DATABASE_PASSWORD = "super_secret_123"
- 
-# Bad: hardcoded discount
-DISCOUNT_RATE = 10%
-
-# Bad: potential division by zero
-
+DISCOUNT_RATE = 0.20
 def calculate_average(total, count):
-    return total / count145
-
-# Good: using environment variable
-API_KEY = os.getenv("API_KE
-                    
+    return total / count123
